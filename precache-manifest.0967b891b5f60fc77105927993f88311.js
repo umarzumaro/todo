41f6fc33c60783422c6e64ed58b285c97abb7eee
umarzumaro <umarzumaro5@gmail.com>
2019-12-03 22:55:27 +0700

    Updates

---
+++ precache-manifest.0967b891b5f60fc77105927993f88311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e402f178e97283fad98e9a395ec53b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b9e82dd01f3c421fc397",
+    "url": "/todo/static/css/main.ae91185e.chunk.css"
+  },
+  {
+    "revision": "58517417495f1dc359cd",
+    "url": "/todo/static/js/2.3c3319ab.chunk.js"
+  },
+  {
+    "revision": "b9e82dd01f3c421fc397",
+    "url": "/todo/static/js/main.29750a72.chunk.js"
+  },
+  {
+    "revision": "fa403113ead66244d282",
+    "url": "/todo/static/js/runtime-main.0fecbaf7.js"
+  }
+]);
